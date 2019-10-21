7096454c5be958657cb6063f3e484566932e42c4
jdmejiav <jdmejiav@eafit.edu.co>
2019-10-20 20:12:15 -0500

    purge

--- Graphs/src/Grafo.java
+++ Graphs/src/Grafo.java
@@ -1,14 +1,18 @@
 
 
-
+import java.util.Hashtable;
 
 public class Grafo{
 
+	private Hashtable <Integer,Integer>posiciones_prohibidas;
+	private String []initial_postion;
+	private String []forbidenPositions;
+	public Grafo(String forbidden_positions[],String initial_position[]){
+		this.initial_postion=initial_position;
+		this.forbidenPositions=forbidden_positions;
 
-	private int initialPostion;
-	private int []forbidenPositions;
-	public Grafo(int forbiddenPositions[],int initialPosition){
-		this.initialPosition=initialPosition;
-		this.forbiddenPositions=forbiddenPositions;
 	}
+
+
+
 }
--- Graphs/src/Main.java
+++ Graphs/src/Main.java
@@ -1,33 +1,47 @@
 
-import java.io.file;
+import java.io.File;
 import java.util.Scanner;
 public class Main {
 	public static void main (String []args){
-		leerArchivo();
+		Main main = new Main();
+		main.leer_archivo();
 	}
-	private void leerArchivo(){
+	private void leer_archivo(){
 		try{
 			File file = new File("Entrada.in");
 			Scanner sc = new Scanner (file);
-			int nTestCases = sc.nextInt();
+			int n_test_cases = sc.nextInt();
 			while (sc.hasNextLine()){
-				String [] testCases=leerTestCases(nTestCases,sc);
-				
+				String []test_cases=leerTestCases(n_test_cases,sc);
+				int n_forbidden_cases=sc.nextInt();
+				String []forbidden_positions= forbidden_positions(n_forbidden_cases,sc);
+				Grafo gr = new Grafo(test_cases,forbidden_positions);
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-	private []String leerTestCases(int testCases,Scanner sc){
-		String retorno [] = new String [testC];
+	private String[] leerTestCases(int testCases,Scanner sc){
+		String retorno [] = new String [testCases];
 		for (int i=0;i<testCases;i++){
-			retono[i]=sc.nextLine();
+			retorno[i]=sc.nextLine();
 		}
 		return retorno;
 	}
-	private String forbiddenPositions(){
-		
 
+	private String[] forbidden_positions(int forbidden_positions,Scanner sc){
+		String retorno [] = new String [forbidden_positions];
+		for (int i=0;i<forbidden_positions;i++){
+			retorno[i]=sc.nextLine();
+		}
+		return retorno;
+	}
+
+
+
+
+	private String forbiddenPositions(){
+	return "";
 	}
 }
 
